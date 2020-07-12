8b1ee8fc0ec9852c243767124f435fd81ad4bdf6
yuannwang8 <yuannwang8@users.noreply.github.com>
2020-07-13 00:03:57 +0800

    updated angular distance calculations

--- GeocentricTransit.py
+++ GeocentricTransit.py
@@ -1,5 +1,7 @@
 from astroquery.jplhorizons import Horizons
 from astropy.table import Table, join
+from astropy import units as u
+from astropy.coordinates import SkyCoord, Angle
 import numpy as np
 
 ## https://docs.astropy.org/en/stable/api/astropy.table.Table.html
@@ -28,9 +30,9 @@ def downloadSolarDiskFlat():
     print('downloading Overview Disk files needed from Horizons')
     # epochs_use = {'start':'2084-11-10 02:00', 'stop':'2084-11-10 13:05', 'step':'39900'}
     epochs_use = {'start':'2084-11-10 02:03', 'stop':'2084-11-10 13:02', 'step':'39540'}
-    ephT1 = dl_flats('T', '399','500@499', epochs_use, '2,12,13,23')
-    ephS1 = dl_flats('S', '10','500@499', epochs_use, '2,13')
-    ephL1 = dl_flats('L', '301','500@499', epochs_use, '2,13,23')
+    ephT1 = dl_flats('T', '399','500@499', epochs_use, '2,12,13,23') # Earth from Mars geocentric
+    ephS1 = dl_flats('S', '10','500@499', epochs_use, '2,13') # Sun from Mars geocentric
+    ephL1 = dl_flats('L', '301','500@499', epochs_use, '2,13,23') # Moon from Mars geocentric
     eph = join(ephT1, ephS1, keys = ['datetime_str','datetime_jd'])
     eph = join(eph, ephL1, keys = ['datetime_str','datetime_jd'])
     # type(eph); print(eph.colnames)
@@ -47,13 +49,40 @@ def readSolarDiskFlat():
     print('read downloaded files for Overview Disc')
     return Table.read('ephTSL1.ecsv', format = 'ascii.ecsv')
 
+def angular_dist_arcsec( ra1, dec1, ra2, dec2):
+    '''
+    Takes RA and DEC in radian form and return the angular distance in ~radian~ arcsec form
+    d-ang = 2 arcsin * sqrt(A + B), where
+    A = sin2(abs(DEC1 - DEC2)/2) and B = cos(DEC1)cos(DEC2)sin2(abs(RA1 - RA2)/2)
+    Also, 1 radian = (180/np.pi) degree = 3600 * (180/np.pi) arcsec
+    '''
+    A = np.sin(np.abs(dec1 - dec2)/2)**2
+    B = np.cos(dec1)*np.cos(dec2)*np.sin(np.abs(ra1 - ra2)/2)**2
+    AngDist = 2*np.arcsin(np.sqrt(A + B))*(180*3600)/np.pi
+    return AngDist
+
+def cal_angdist(eph, transiting_objects):
+    '''
+    Calculate Angular Distances between Sun and Objects, in arcsec
+    '''
+    ephnew = eph.copy()
+    Solar_RA = np.radians(ephnew['S_RA_app'])
+    Solar_DEC = np.radians(ephnew['S_DEC_app'])
+    for tobject in transiting_objects:
+        this_RA = tobject + '_RA_app'
+        this_RA = np.radians(ephnew[this_RA])
+        this_DEC = tobject + '_DEC_app'
+        this_DEC = np.radians(ephnew[this_DEC])
+        this_AngDist = tobject + 'S_angsep'
+        ephnew[this_AngDist] = angular_dist_arcsec(Solar_RA, Solar_DEC, this_RA, this_DEC) * u.arcsec
+    return ephnew
 
 def solar_InEgress(eph, transiting_objects):
     '''
     Identifying when an object transits the solar disk
     Method
-    # Outer Ingress/Egress: SOT < object ang radius + solar ang radius
-    # Inner Ingress/Egress: SOT < object ang radius - solar and radius
+    # Outer Ingress/Egress: xS_angsep < object ang radius + solar ang radius
+    # Inner Ingress/Egress: xS_angsep < object ang radius - solar and radius
     # Point of Greatest Transit:
     ## For Terra, Angular Separation at minimum as this info is available
     ## In general, where SOT is minimum
@@ -61,24 +90,22 @@ def solar_InEgress(eph, transiting_objects):
     # Where multiple rows are returned, take the median row (or the earlier median row if number of rows are even)
     '''
     print('Identifying interesting events')
-    ### Terra Min Angular Separation
-    ephnew = eph.copy()
-    nptTMinAngSep = ephnew['datetime_str','datetime_jd'][ephnew['T_sat_sep'] == np.min(ephnew['T_sat_sep'])].copy()
-    i = int(np.floor(len(nptTMinAngSep)/2))-1
-    tA = ['T'] ; tB = ['MinAngSep'] ; tC = [nptTMinAngSep['datetime_str'][i]] ; tD = [nptTMinAngSep['datetime_jd'][i]]
     ### Terra Direct Masking by way of T_sat_vis = t
     ephnew = eph.copy()
     mask = ephnew['T_sat_vis'] == 't'
+    tA = []; tB = []; tC = []; tD = []
     tA.append('T'); tB.append('Tflag_start'); tC.append(ephnew['datetime_str'][mask][0]); tD.append(ephnew['datetime_jd'][mask][0])
     tA.append('T'); tB.append('Tflag_end'); tC.append(ephnew['datetime_str'][mask][len(ephnew[mask])-1]); tD.append(ephnew['datetime_jd'][mask][len(ephnew[mask])-1])
     ### My Calculations
     ephnew = eph.copy()
     for tobject in transiting_objects:
         this_width = tobject + '_ang_width'
-        this_SOT = tobject + '_elong'
+        this_angsep = tobject + 'S_angsep'
+        #this_SOT = tobject + '_elong'
         ### Outer Transit boundaries
         Touter = tobject + 'S_Touter'
-        ephnew[Touter] = 3600*ephnew[this_SOT] < (ephnew['S_ang_width'] + ephnew[this_width])/2
+        # ephnew[Touter] = 3600*ephnew[this_SOT] < (ephnew['S_ang_width'] + ephnew[this_width])/2
+        ephnew[Touter] = ephnew[this_angsep] < (ephnew['S_ang_width'] + ephnew[this_width])/2
         nptTouter = ephnew['datetime_str','datetime_jd'][ephnew[Touter]].copy()
         a = tobject; b = 'Outer_Ingress'; c = nptTouter['datetime_str'][0]; d = nptTouter['datetime_jd'][0]
         tA.append(a); tB.append(b); tC.append(c); tD.append(d)
@@ -86,33 +113,30 @@ def solar_InEgress(eph, transiting_objects):
         tA.append(a); tB.append(b); tC.append(c); tD.append(d)
         ### Inner Transit boundaries
         Tinner = tobject + 'S_Tinner'
-        ephnew[Tinner] = 3600*ephnew[this_SOT] < (ephnew['S_ang_width'] - ephnew[this_width])/2
+        # ephnew[Tinner] = 3600*ephnew[this_SOT] < (ephnew['S_ang_width'] - ephnew[this_width])/2
+        ephnew[Tinner] = ephnew[this_angsep] < (ephnew['S_ang_width'] - ephnew[this_width])/2
         nptTinner = ephnew['datetime_str','datetime_jd'][ephnew[Tinner]].copy()
         a = tobject; b = 'Inner_Ingress'; c = nptTinner['datetime_str'][0]; d = nptTinner['datetime_jd'][0]
         tA.append(a); tB.append(b); tC.append(c); tD.append(d)
         a = tobject; b = 'Inner_Egress'; c = nptTinner['datetime_str'][len(nptTinner)-1]; d = nptTinner['datetime_jd'][len(nptTinner)-1]
         tA.append(a); tB.append(b); tC.append(c); tD.append(d)
         ### Greatest Transit (i.e. min SOT)
-        nptmSOT = ephnew['datetime_str','datetime_jd'][ephnew[this_SOT] == np.min(ephnew[this_SOT])].copy()
-        i = int(np.floor(len(nptmSOT)/2))-1
+        #nptmSOT = ephnew['datetime_str','datetime_jd'][ephnew[this_SOT] == np.min(ephnew[this_SOT])].copy()
+        nptmSOT = ephnew['datetime_str','datetime_jd'][ephnew[this_angsep] == np.min(ephnew[this_angsep])].copy()
+        i = max(int(np.floor(len(nptmSOT)/2))-1,0)
         a = tobject; b = 'Greatest_Transit'; c = nptmSOT['datetime_str'][i]; d = nptmSOT['datetime_jd'][i]
         tA.append(a); tB.append(b); tC.append(c); tD.append(d)
     ### Photo Opportunity
     ephnew = eph.copy()
-    ephnew['TL_SOTs'] = ephnew['T_elong'] + ephnew['L_elong']
-    ephnew['TL_dist'] = np.sqrt(np.square(ephnew['T_RA_app']-ephnew['S_RA_app'])+np.square(ephnew['T_DEC_app']-ephnew['S_DEC_app'])) + np.sqrt(np.square(ephnew['L_RA_app']-ephnew['S_RA_app'])+np.square(ephnew['L_DEC_app']-ephnew['S_DEC_app']))
-    nptPhoto1 = ephnew['datetime_str','datetime_jd'][ephnew['TL_SOTs'] == np.min(ephnew['TL_SOTs'])].copy()
-    i = int(np.floor(len(nptPhoto1)/2))-1
-    a = 'TL'; b = 'Photo_minSOTs'; c = nptPhoto1['datetime_str'][i]; d = nptPhoto1['datetime_jd'][i]
-    tA.append(a); tB.append(b); tC.append(c); tD.append(d)
-    nptPhoto2 = ephnew['datetime_str','datetime_jd'][ephnew['TL_dist'] == np.min(ephnew['TL_dist'])].copy()
-    i = int(np.floor(len(nptPhoto2)/2))-1
-    a = 'TL'; b = 'Photo_minDists'; c = nptPhoto2['datetime_str'][i]; d = nptPhoto2['datetime_jd'][i]
+    ephnew['TL_adist'] = ephnew['TS_angsep'] + ephnew['LS_angsep']
+    nptPhoto = ephnew['datetime_str','datetime_jd'][ephnew['TL_adist'] == np.min(ephnew['TL_adist'])].copy()
+    i = max(int(np.floor(len(nptPhoto)/2))-1,0)
+    a = 'TL'; b = 'Photo_Op'; c = nptPhoto['datetime_str'][i]; d = nptPhoto['datetime_jd'][i]
     tA.append(a); tB.append(b); tC.append(c); tD.append(d)
     ### Sort output table
     Tout = Table([tA, tB, tC, tD], names=('Object','Event','datetime_str','datetime_jd'))
     Tout.sort('datetime_jd')
-    returnthese = ['datetime_jd','datetime_str', '_ang_width', '_RA_app', '_DEC_app']
+    returnthese = ['datetime_jd','datetime_str','_ang_width','_angsep','_RA_app','_DEC_app']
     returncol = [s for s in list(ephnew.columns) if any(map(s.endswith, returnthese))]
     plottable = join(Tout, ephnew[returncol])
     print('Writing Event Table')
@@ -126,21 +150,19 @@ def solar_InEgress(eph, transiting_objects):
 # eph = downloadSolarDiskFlat()
 eph = readSolarDiskFlat()
 
+### Calculate angular distances between sun and transiting objects
+# eph2 = cal_angdist(eph, ['T','L']) # your own equations
+# use build in from SkyCoord instead
+eph2 = eph.copy()
+Solar = SkyCoord(eph['S_RA_app'], eph['S_DEC_app'], frame = 'icrs', unit = 'deg')
+Terra = SkyCoord(eph['T_RA_app'], eph['T_DEC_app'], frame = 'icrs', unit = 'deg')
+Luna = SkyCoord(eph['L_RA_app'], eph['L_DEC_app'], frame = 'icrs', unit = 'deg')
+eph2['TS_angsep'] = Solar.separation(Terra).arcsec * u.arcsec
+eph2['LS_angsep'] = Solar.separation(Luna).arcsec * u.arcsec
+
 ### Generate Summary Events
-SummaryDiskEvents, plottable = solar_InEgress(eph, ['T', 'L'])
+SummaryDiskEvents, plottable = solar_InEgress(eph2, ['T', 'L'])
 SummaryDiskEvents.pprint_all()
 plottable.pprint_all()
 
 # eyeball values: print(eph[eph['datetime_jd'] == 2482540.039189815])
-
-
-### Size of Disks
-# print('\nSize of Disks:')
-# print('\nMax delta of Solar angular diameter in arcsec over time')
-# print(np.max(eph['S_ang_width']) - np.min(eph['S_ang_width']))
-#
-# print('\nMax delta of Terra angular diameter in arcsec')
-# print(np.max(eph['T_ang_width']) - np.min(eph['T_ang_width']))
-#
-# print('\nMax delta of Lunar angular diameter in arcsec')
-# print(np.max(eph['L_ang_width']) - np.min(eph['L_ang_width']))
